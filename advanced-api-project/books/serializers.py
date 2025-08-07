8132f30f1e89492ca51c9661b9a1404fdfcc1d84
oluwaseun <t.oluwaseunofficial@gmail.com>
2025-08-07 06:29:37 -0400

    Implement working book API

--- advanced-api-project/books/serializers.py
+++ advanced-api-project/books/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Author, Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
 
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)  # ← This is what ALX is looking for
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
