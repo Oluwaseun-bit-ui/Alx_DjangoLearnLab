c0cc8d004e03a51db5a2322d57ee798a1ac24b04
oluwaseun <t.oluwaseunofficial@gmail.com>
2025-08-07 06:17:59 -0400

    Implement working book API endpoint

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,7 +1,8 @@
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path, include  # You need include to connect app URLs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # This connects to your api/urls.py
-]
+    path('api/', include('api.urls')), 
+    path('books/', include('books.urls')),  
+]  
--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from .models import Author, Book
 
+
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['id', 'name']
+        fields = '__all__'
+
 
 class BookSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()  # Nested representation of the author
-
     class Meta:
         model = Book
-        fields = ['id', 'title', 'publication_year', 'author']
-        
+        fields = '__all__'
+
+    def validate_title(self, value):
+        """Custom validation to ensure the book title is not too short."""
+        if len(value) < 3:
+            raise serializers.ValidationError("Title must be at least 3 characters long.")
+        return value
