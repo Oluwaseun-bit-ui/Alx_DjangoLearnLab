74355a5686dc1bcf6c95cc43b8e051697bb208d1
oluwaseun <t.oluwaseunofficial@gmail.com>
2025-08-23 14:47:40 -0400

    Setup Social Media API with authentication, posts, comments, and profiles

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    # One field handles both followers and following relationships
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
 
     def __str__(self):
         return self.username
