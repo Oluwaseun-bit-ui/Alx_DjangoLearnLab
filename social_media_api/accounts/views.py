8d467c592b00e10bd6e38daddce56508acdc7c34
oluwaseun <t.oluwaseunofficial@gmail.com>
2025-09-01 17:20:50 -0400

    updated the social_media_api/accounts/views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,10 @@
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from .models import CustomUser
 
 class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
@@ -21,7 +20,7 @@ class FollowUserView(generics.GenericAPIView):
 
 class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
